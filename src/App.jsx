f8f29feefb0044dac7cbdcdb4b052b1a4cc2dd22
Etienne Marques <97489119+etmarques@users.noreply.github.com>
2023-06-29 14:14:21 -0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
+// rotas
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-
 
 // pages
 import Home from '../src/pages/Home';
 import Form from '../src/pages/Form';
 import Matriz from '../src/pages/Matriz';
 
+//Msal from https://learn.microsoft.com/pt-br/azure/active-directory/develop/single-page-app-tutorial-04-call-api?tabs=visual-studio-code
+
+import { loginRequest } from './authConfig';
+import { callMsGraph } from './graph';
+import { ProfileData } from './components/ProfileData';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import { Button } from 'react-bootstrap';
+
+/**
+* Renders information about the signed-in user or a button to retrieve data about the user
+*/
+/**
+* Renders information about the signed-in user or a button to retrieve data about the user
+
+const ProfileContent = () => {
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
+
+  function RequestProfileData() {
+    // Silently acquires an access token which is then attached to a request for MS Graph data
+    instance
+      .acquireTokenSilent({
+        ...loginRequest,
+        account: accounts[0],
+      })
+      .then((response) => {
+        callMsGraph(response.accessToken).then((response) => setGraphData(response));
+      });
+  }
+
+  return (
+    <>
+      <h5 className="card-title">Welcome {accounts[0].name}</h5>
+      <br />
+      {graphData ? (
+        <ProfileData graphData={graphData} />
+      ) : (
+        <Button variant="secondary" onClick={RequestProfileData}>
+          Request Profile Information
+        </Button>
+      )}
+    </>
+  );
+};
+*/
+//criação das rotas
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
@@ -16,10 +62,13 @@ const router = createBrowserRouter(
   )
 )
 
+
+
 function App() {
   return (
-
+    <>
     <RouterProvider router={router} />
+    </>
     
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,9 +3,13 @@ import { Container , Nav , Navbar , NavDropdown} from 'react-bootstrap'
 import logo from '../assets/img/trouverLogoBlack.svg'
 import { NavLink as LinkRoute} from 'react-router-dom'
 
+import { useIsAuthenticated } from "@azure/msal-react";
+import { SignInButton } from "../components/SignInButton";
+import { SignOutButton } from "./SignOutButton";
 
 const Menu = () => {
 
+    const isAuthenticated = useIsAuthenticated();
     const classMenuItem = 'text-decoration-none text-secondary navLinkItem'
 
     return (    
@@ -43,8 +47,8 @@ const Menu = () => {
                                 <LinkRoute className={`${classMenuItem} ps-5 mt-2`} to='#'>Matriz</LinkRoute>
                                 <LinkRoute className={`${classMenuItem} ps-5 mt-2`} to='#'>Matriz</LinkRoute>
                             </NavDropdown>
-                            <LinkRoute className={`${classMenuItem} pb-3 mt-2`} to='#'>Sair</LinkRoute>
                             <LinkRoute className={`${classMenuItem} pb-2`} to='#'>Ajuda</LinkRoute>
+                            {isAuthenticated ? <SignOutButton /> : <SignInButton />}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
